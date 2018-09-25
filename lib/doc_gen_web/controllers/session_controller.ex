d3078d18d41b5667831cf1199c205143eae91e93
Michael Davis <mcd164@miami.edu>
2018-09-25 00:46:15 -0400

    Begin writing session and auth stuff

---
+++ lib/doc_gen_web/controllers/session_controller.ex
@@ -0,0 +1,27 @@
+defmodule DocGenWeb.SessionController do
+  use DocGenWeb, :controller
+
+  alias DocGenWeb.Plugs.Auth
+
+  def new(conn, _), do: render(conn, "new.html")
+
+  def create(conn, %{"session" => %{"username" => u, "password" => pass}}) do
+    case Auth.login(conn, u, pass) do
+      {:ok, conn} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> redirect(to: Routes.page_path(conn, :index))
+
+      {:error, _reason, conn} ->
+        conn
+        |> put_flash(:error, "Invalid username/password combination")
+        |> render("new.html")
+    end
+  end
+
+  def delete(conn, _) do
+    conn
+    |> Auth.logout()
+    |> redirect(to: Routes.page_path(conn, :index))
+  end
+end
---
+++ lib/doc_gen_web/plugs/auth.ex
@@ -0,0 +1,36 @@
+defmodule MoleWeb.Plugs.Auth do
+  @moduledoc false
+  import Plug.Conn
+
+  alias Mole.Accounts
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    user_id = get_session(conn, :user_id)
+    user = user_id && Accounts.get_user(user_id)
+    assign(conn, :current_user, user)
+  end
+
+  def login(conn, uname, given_pass) do
+    case Accounts.authenticate(uname, given_pass) do
+      {:ok, user} ->
+        {:ok, login(conn, user)}
+
+      {:error, :unauthorized} ->
+        {:error, :unauthorized, conn}
+
+      {:error, :not_found} ->
+        {:error, :not_found, conn}
+    end
+  end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
+
+  def logout(conn), do: configure_session(conn, drop: true)
+end
