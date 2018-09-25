67c2a3d2a31fbb1b636acd75132d224e2b7159a2
Michael Davis <mcd164@miami.edu>
2018-09-24 22:03:37 -0400

    Define the child spec for ecto

--- lib/doc_gen/repo.ex
+++ lib/doc_gen/repo.ex
@@ -1,5 +1,15 @@
 defmodule DocGen.Repo do
   use Ecto.Repo,
     otp_app: :doc_gen,
-    adapter: Ecto.Adapters.Postgres
+    adapter: EctoMnesia.Adapter
+
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]},
+      type: :worker,
+      restart: :permanent,
+      shutdown: 500
+    }
+  end
 end
