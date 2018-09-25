080fff51dc3b30f8ca0f40d1227b5aeaf23a1808
Michael Davis <mcd164@miami.edu>
2018-09-24 20:38:50 -0400

    Create a scope for admin stuff

--- lib/doc_gen_web/router.ex
+++ lib/doc_gen_web/router.ex
@@ -14,14 +14,12 @@ defmodule DocGenWeb.Router do
   end
 
   scope "/", DocGenWeb do
-    # Use the default browser stack
     pipe_through(:browser)
 
     get("/", PageController, :index)
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", DocGenWeb do
-  #   pipe_through :api
-  # end
+  scope "/admin", DocGenWeb do
+    pipe_through(:browser)
+  end
 end
