b05ec802cd2a19fd4eebe029444a964e857013e2
Michael Davis <mcd164@miami.edu>
2018-09-24 20:28:31 -0400

    Format

--- config/config.exs
+++ config/config.exs
@@ -3,15 +3,14 @@ use Mix.Config
 config :doc_gen,
   ecto_repos: [DocGen.Repo]
 
-config :doc_gen, DocGen.Repo,
-  adapter: EctoMnesia.Adapter
+config :doc_gen, DocGen.Repo, adapter: EctoMnesia.Adapter
 
 config :doc_gen, DocGenWeb.Endpoint,
   url: [host: "localhost"],
-  secret_key_base: "Pb0S1nsqJf1VVTKNVlPyIoPzyAnrP/RQk8XnMeAP88x39z2f5l8AGsEQMmsEJS+y",
+  secret_key_base:
+    "Pb0S1nsqJf1VVTKNVlPyIoPzyAnrP/RQk8XnMeAP88x39z2f5l8AGsEQMmsEJS+y",
   render_errors: [view: DocGenWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: DocGen.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: DocGen.PubSub, adapter: Phoenix.PubSub.PG2]
 
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
@@ -26,4 +25,4 @@ config :ecto_mnesia,
   host: {:system, :atom, "MNESIA_HOST", Kernel.node()},
   storage_type: {:system, :atom, "MNESIA_STORAGE_TYPE", :disc_copies}
 
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
--- config/dev.exs
+++ config/dev.exs
@@ -5,8 +5,15 @@ config :doc_gen, DocGenWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/webpack/bin/webpack.js", "--mode", "development", "--watch-stdin",
-                    cd: Path.expand("../assets", __DIR__)]]
+  watchers: [
+    node: [
+      "node_modules/webpack/bin/webpack.js",
+      "--mode",
+      "development",
+      "--watch-stdin",
+      cd: Path.expand("../assets", __DIR__)
+    ]
+  ]
 
 config :doc_gen, DocGenWeb.Endpoint,
   live_reload: [
