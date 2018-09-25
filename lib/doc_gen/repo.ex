2d7a7e50d1e715496017d64ca18964fd763174cc
Michael Davis <mcd164@miami.edu>
2018-09-25 18:11:57 -0400

    Add the missing child spec

--- lib/doc_gen/repo.ex
+++ lib/doc_gen/repo.ex
@@ -2,4 +2,14 @@ defmodule DocGen.Repo do
   use Ecto.Repo,
     otp_app: :doc_gen,
     adapter: Ecto.Adapters.Postgres
+
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]},
+      type: :worker,
+      restart: :permanent,
+      shutdown: 500
+    }
+  end
 end
