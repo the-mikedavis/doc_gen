034e62fbd46260c860f217a4607c5454f31f8e26
Michael Davis <mcd164@miami.edu>
2018-09-26 09:52:40 -0400

    Add Sass

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -29,13 +29,13 @@ module.exports = (env, options) => ({
         }
       },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        test: /\.s?[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       }
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: '../css/app.css' }),
+    new MiniCssExtractPlugin({ filename: '../css/app.sass' }),
     new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
   ]
 });
