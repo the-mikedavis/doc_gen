467fab2e3c2f8608b9e4a11f3e81f0811cd2ec06
Michael Davis <mcd164@miami.edu>
2018-09-26 14:30:20 -0400

    Allow 400 MB uploads

--- lib/doc_gen_web/endpoint.ex
+++ lib/doc_gen_web/endpoint.ex
@@ -31,7 +31,8 @@ defmodule DocGenWeb.Endpoint do
   plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
-    json_decoder: Phoenix.json_library()
+    json_decoder: Phoenix.json_library(),
+    length: 400_000_000
   )
 
   plug(Plug.MethodOverride)
