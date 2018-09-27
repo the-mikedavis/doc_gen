45c467240e695b4fa8ce7d3df9f4f54e2e5623ed
Michael Davis <mcd164@miami.edu>
2018-09-27 19:38:01 -0400

    Add tags

---
+++ priv/repo/migrations/20180927232937_create_tags.exs
@@ -0,0 +1,12 @@
+defmodule DocGen.Repo.Migrations.CreateTags do
+  use Ecto.Migration
+
+  def change do
+    create table(:tags) do
+      add :name, :string
+
+      timestamps()
+    end
+
+  end
+end
