f1e62bd252a5413b9b3d8f7d80811ee6fc0bdb2e
Michael Davis <mcd164@miami.edu>
2018-09-28 05:05:56 +0000

    Take out the dashboard stuff, use videos instead

--- lib/doc_gen_web/controllers/dashboard_controller.ex
+++
@@ -1,8 +0,0 @@
-defmodule DocGenWeb.DashboardController do
-  use DocGenWeb, :controller
-
-  def index(conn, _params) do
-    # TODO: assign videos and tags and such
-    render(conn, "index.html")
-  end
-end
