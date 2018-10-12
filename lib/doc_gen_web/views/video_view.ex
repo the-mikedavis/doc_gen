0891eeb566133d4931d2262505795e10548f4120
Michael Davis <mcd164@miami.edu>
2018-10-12 13:39:06 -0400

    Show the video duration and weight

--- lib/doc_gen_web/views/video_view.ex
+++ lib/doc_gen_web/views/video_view.ex
@@ -2,4 +2,11 @@ defmodule DocGenWeb.VideoView do
   use DocGenWeb, :view
 
   def title(_, _), do: "Doc-Gen: Dashboard"
+
+  def duration(seconds) do
+    minutes = Integer.floor_div(seconds, 60)
+    rem_secs = rem(seconds, 60)
+
+    "#{minutes}:#{rem_secs}"
+  end
 end
