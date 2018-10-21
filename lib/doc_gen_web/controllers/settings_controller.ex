5d39aa3879284b8cc5e95b25c1baab600dfb5a95
Michael Davis <mcd164@miami.edu>
2018-10-21 05:43:01 +0000

    Put the tags into the settings controller assigns

--- lib/doc_gen_web/controllers/settings_controller.ex
+++ lib/doc_gen_web/controllers/settings_controller.ex
@@ -8,13 +8,14 @@ defmodule DocGenWeb.SettingsController do
     settings = Accounts.get_settings()
     changeset = Accounts.change_settings(settings)
     video_count = Content.count_videos()
+    tags = Content.list_tags()
 
     if video_count < settings.length do
       put_flash(conn, :error, "There aren't enough videos for #{settings.length} clips!")
     else
       conn
     end
-    |> render("index.html", changeset: changeset, settings: settings)
+    |> render("index.html", changeset: changeset, settings: settings, tags: tags)
   end
 
   def update(conn, %{"id" => id, "setting" => setting_params}) do
