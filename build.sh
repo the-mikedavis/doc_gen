ed475c66b2e68c5796fefce76e05fb6c28e5336d
Michael Davis <mcd164@miami.edu>
2018-10-24 04:24:40 +0000

    Build script for deploys

---
+++ build.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -e
+
+if [[ $# -eq 0 ]]; then
+  echo 'Supply a tag as the first argument!'
+  exit 1
+fi
+
+mix deps.get
+mix bless
+cd assets
+./node_modules/.bin/webpack -p
+cd ..
+MIX_ENV=prod mix phx.digest
+MIX_ENV=prod mix release --env=prod
+~/go/bin/ghr $1 ./_build/prod/rel/doc_gen/releases/*/*.tar.gz
