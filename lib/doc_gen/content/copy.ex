92ecd66186000d64e1658a6edd0dd6d8a2493a0c
Michael Davis <mcd164@miami.edu>
2019-02-08 19:12:15 -0500

    Format

--- lib/doc_gen/content/copy.ex
+++ lib/doc_gen/content/copy.ex
@@ -18,7 +18,11 @@ defmodule DocGen.Content.Copy do
     Agent.update(__MODULE__, fn _t ->
       settings = Accounts.get_settings()
 
-      %{settings | copy: Earmark.as_html!(settings.copy), about: Earmark.as_html!(settings.about)}
+      %{
+        settings
+        | copy: Earmark.as_html!(settings.copy),
+          about: Earmark.as_html!(settings.about)
+      }
     end)
   end
 
